41d64018f6330e7c19743095ff57b161c32f04ac
Carl <carl.last@gmail.com>
2024-11-20 15:05:45 +0000

    async working

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var request = require("request");
 var exec = require('child_process').exec;
+const fs = require('fs');
 
 var upstarttag = '<span style="background-color: #00FF00;padding: 10px;">';
 var downstarttag = '<span style="background-color: #FF0000;padding: 10px;">';
 var endtag = '</span>';
+let opfile = 'servicecheck.html';
 
 var checks = [
   { "name": "INF TEST BIP", "type": "url", "target": "http://10.0.1.5:7001/xmlpserver", "expect": "BI Publisher Enterprise" }
@@ -14,34 +16,53 @@ var checks = [
   , { "name": "SYS TEST DB", "type": "ora", "target": "sys/delta1@10.0.1.22/centos7.dbaora.com as sysdba", "expect": "DATABASE_IS_UP" }
 ];
 
-function itsUP(name) {
-  console.log(upstarttag + name + endtag);
-}
-
-function itsDOWN(name) {
-  console.log(downstarttag + name + endtag);
-}
-
-checks.forEach((element) => {
-  if (element.type == "url") {
+function checkURI(element) {
+  return new Promise(function (resolve, reject) {
     request({ uri: element.target },
       function (error, response, body) {
-        if (body.indexOf(element.expect) > 0) {
-          itsUP(element.name);
+        if (body != undefined && body.indexOf(element.expect) > 0) {
+          resolve(upstarttag + element.name + endtag);
         } else {
-          itsDOWN(element.name);
+          resolve(downstarttag + element.name + endtag);
         }
       });
-  }
-  if (element.type == "ora") {
-    var child;
-    child = exec('echo "PROMPT DATABSE_IS_UP" | sqlplus -S -L '+element.target, { timeout: 10000 }, 
-      function (error, stdout, stderr) { 
-        if ( stdout == 'DATABSE_IS_UP' ) { 
-          itsUP(element.name);
+  });
+}
+
+function checkORA(element) {
+  return new Promise(function (resolve, reject) {
+    child = exec('echo "PROMPT DATABASE_IS_UP" | sqlplus -S -L ' + element.target, { timeout: 10000 },
+      function (error, stdout, stderr) {
+        if (stdout.substring(0,14) == 'DATABASE_IS_UP') {
+          resolve(upstarttag + element.name + endtag);
         } else {
-          itsDOWN(element.name);
+          resolve(downstarttag + element.name + endtag);
         }
-      });
+      }
+    );
+  });
+}
+
+async function main() {
+  try {
+    let response = '';
+    fs.copyFileSync('topsc.html', opfile);
+    for (i = 0; i < checks.length; i++) {
+      if (checks[i].type == "url") {
+        let retval = await checkURI(checks[i]);
+        response = response + retval + '\n';
+      }
+      if (checks[i].type == "ora") {
+        let retval = await checkORA(checks[i]);
+        response = response + retval + '\n';
+      }
+    }
+    fs.appendFileSync(opfile, response);
+    fs.appendFileSync(opfile, '<h2>END OF REPORT</h2>');
+  } catch (error) {
+    fs.appendFileSync(opfile, error);
+    fs.appendFileSync(opfile, '<h2>END OF REPORT</h2></body>');
   }
-});
+}
+
+main();
